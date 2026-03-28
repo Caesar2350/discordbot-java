e6a42620ef0d4a524db672e1b97ef32eb5e8d65c
mohit <mohitjoshi2502@gmail.com>
2026-03-29 01:59:51 +0530

    Remove API key and use env variables

--- src/main/java/org/example/ai/GeminiService.java
+++ src/main/java/org/example/ai/GeminiService.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class GeminiService {
 
-    private static final String API_KEY = System.getenv("AIzaSyBhuvL18rPxpqFRFGeEjrIzz2GepLXJhcM");
+    private static final String API_KEY = System.getenv("API_KEY");
 
     public static String getResponse(String prompt) {
         try {
